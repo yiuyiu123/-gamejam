8898cdd146e11bbb60b13c02fa217c0426384533
ZhangYixin <zyxyy060404@qq.com>
2025-10-24 11:45:05 +0800

    scene3跳转时机正确

---
+++ 3dtest/Assets/脚本/scene5/IsSwitch.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsSwitch : MonoBehaviour
+{
+    [Header("UI—°œÓ")]
+    public GameObject UI_Yes;
+    public GameObject UI_NO;
+
+    [Header("UI’⁄’÷")]
+    public GameObject UI_mask1;
+    public GameObject UI_mask2;
+
+    private void Start()
+    {
+        UI_mask1.SetActive(false);
+        UI_mask2.SetActive(false);
+    }
+
+    public void ChooseYes()
+    {
+        UI_mask1.SetActive(true);
+    }
+
+    public void ChooseNo()
+    {
+        UI_mask2.SetActive(true);
+    }
+}
